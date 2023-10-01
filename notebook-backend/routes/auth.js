4796b7e00850828fe68ac83a3fa83d96df64f0a9
Shashank K Prabhu <shashankkp02@gmail.com>
2023-10-01 21:13:08 +0530

    Storing data inside mongodb using mongoose model

---
+++ notebook-backend/routes/auth.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const router = express.Router();
+const user = require('../models/User');
+
+//Create a user using :POST "/api/auth/".Doesn't require Auth.
+router.post('/',(req,res)=>{  
+  console.log(req.body);
+  const User =user(req.body);
+  User.save()
+  res.send(req.body);
+})
+module.exports = router;
