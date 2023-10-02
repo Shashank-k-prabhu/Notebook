30f19ef6dcf357710281e960dd0737296e832bd7
Shashank K Prabhu <shashankkp02@gmail.com>
2023-10-02 12:58:10 +0530

    Data validation added

--- notebook-backend/routes/auth.js
+++ notebook-backend/routes/auth.js
@@ -1,12 +1,33 @@
 const express = require("express");
 const router = express.Router();
 const user = require('../models/User');
+const { body, validationResult } = require("express-validator");
+
 
 //Create a user using :POST "/api/auth/".Doesn't require Auth.
-router.post('/',(req,res)=>{  
-  console.log(req.body);
-  const User =user(req.body);
-  User.save()
-  res.send(req.body);
-})
+router.post(
+  "/",
+  [
+    body("name","Enter a valid name").isLength({ min: 3 }),
+    body("email","Enter a valid email").isEmail(),
+    body("password").isLength({ min: 5 }),
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    user.create({
+      name: req.body.name,
+      email: req.body.email,
+      password: req.body.password,
+    })
+    .then((user) => res.json(user))
+    .catch((err)=>console.log(err))
+    res.json({
+      error:"Please enter a unique email address"
+    })
+   
+  }
+);
 module.exports = router;
