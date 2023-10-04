fe2d3b8478cd8e02b341091b2d019fb7c808c722
Shashank K Prabhu <shashankkp02@gmail.com>
2023-10-04 20:34:57 +0530

    Authentication response recieving ,additon of salt and hashing

--- notebook-backend/routes/auth.js
+++ notebook-backend/routes/auth.js
@@ -2,6 +2,9 @@ const express = require("express");
 const router = express.Router();
 const user = require("../models/User");
 const { body, validationResult } = require("express-validator");
+const bcrypt = require("bcryptjs");
+const jwt=require("jsonwebtoken");
+const JWT_SECRET = "shashanknotebook";
 
 //Create a user using :POST "/api/auth/createuser".
 router.post(
@@ -25,14 +28,24 @@ router.post(
           .status(400)
           .json({ error: "User with the same email already exists" });
       }
+      //Securing the password and adding salt to it
+      const salt = await bcrypt.genSaltSync(10);
+      const secPass= await bcrypt.hash(req.body.password,salt);
       //Creates a new user  in DB
       User = await user.create({
         name: req.body.name,
         email: req.body.email,
-        password: req.body.password,
+        password: secPass,
       });
-
-      res.json(User);
+      //JWT token authentication
+      const data={
+        user:{
+          id:User.id
+        }
+      }
+      const authtoken=jwt.sign(data,JWT_SECRET)
+      //res.json(User);
+      res.json({authtoken});
     } catch (error) {
       //Catches error if any their
       console.error(error.message);
