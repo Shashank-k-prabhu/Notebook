07a9ff4fff7e7b66f428519f4ceacda3cc1d6a34
Shashank K Prabhu <shashankkp02@gmail.com>
2023-10-04 19:44:51 +0530

    Validaton of user email and setting up collections in thunder client

--- notebook-backend/routes/auth.js
+++ notebook-backend/routes/auth.js
@@ -1,33 +1,43 @@
 const express = require("express");
 const router = express.Router();
-const user = require('../models/User');
+const user = require("../models/User");
 const { body, validationResult } = require("express-validator");
 
-
-//Create a user using :POST "/api/auth/".Doesn't require Auth.
+//Create a user using :POST "/api/auth/createuser".
 router.post(
   "/",
   [
-    body("name","Enter a valid name").isLength({ min: 3 }),
-    body("email","Enter a valid email").isEmail(),
+    body("name", "Enter a valid name").isLength({ min: 3 }),
+    body("email", "Enter a valid email").isEmail(),
     body("password").isLength({ min: 5 }),
   ],
-  (req, res) => {
+  //If therir are error send the errors as outpkut
+  async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
-    user.create({
-      name: req.body.name,
-      email: req.body.email,
-      password: req.body.password,
-    })
-    .then((user) => res.json(user))
-    .catch((err)=>console.log(err))
-    res.json({
-      error:"Please enter a unique email address"
-    })
-   
+    try {
+      //Checks whethever the user email already exists if esists then sends an error as the funcitons are async as it the other await remains as it is
+      let User = await user.findOne({ email: req.body.email });
+      if (User) {
+        return res
+          .status(400)
+          .json({ error: "User with the same email already exists" });
+      }
+      //Creates a new user  in DB
+      User = await user.create({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
+      });
+
+      res.json(User);
+    } catch (error) {
+      //Catches error if any their
+      console.error(error.message);
+      res.status(500).send("Some error occured");
+    }
   }
 );
 module.exports = router;
