69c9b895ef6006c730c9781b06e74d3ab27b5637
Shashank K Prabhu <shashankkp02@gmail.com>
2024-03-12 18:24:18 +0530

    Trial 3

--- server/middleware/fetchUser.js
+++ server/middleware/fetchUser.js
@@ -1,5 +1,5 @@
 const jwt=require('jsonwebtoken');
-require("dotenv").config({ path: "../.env" });
+require("dotenv").config();
 const JWT_SECRET = process.env.JWT_SECRET;
 const fetchuser=(req,res,next)=>{
   //Get the user from the jwt token and add id  to req object
